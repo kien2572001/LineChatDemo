53a7afa7710b2618b157c8a631786d091073813c
kien2572001 <kien2572001.ntk@gmail.com>
2023-10-11 21:16:33 +0700

    feat: bot auto response

--- app/Http/Controllers/LineWebhookController.php
+++ app/Http/Controllers/LineWebhookController.php
@@ -6,11 +6,12 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Services\LineBotService as LINEBot;
 use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use Illuminate\Support\Facades\Log;
+use App\Models\Message;
 
 class LineWebhookController extends Controller
 {
-    public function message(Request $request)
-    {
+    public function message(Request $request) {
         $data = $request->all();
         $events = $data['events'];
 
@@ -18,8 +19,17 @@ class LineWebhookController extends Controller
         $bot = new LINEBot($httpClient, ['channelSecret' => config('services.line.message.channel_secret')]);
 
         foreach ($events as $event) {
-            $response = $bot->replyText($event['replyToken'], 'メッセージ送信完了');
+            // メッセージの保存処理を追記
+            Message::create([
+                'line_user_id' => $event['source']['userId'],
+                'line_message_id' => $event['message']['id'],
+                'text' => $event['message']['text'],
+            ]);
+             //自動返信が不要であれば削除
+            $response = $bot->replyText($event['replyToken'], "DM Viet");
+            //Log::info($response->getHTTPStatus() . ': ' . $response->getRawBody());
         }
+
         return;
     }
 }
